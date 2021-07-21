5905802973ac6352357b8a9876d5e6b708ba93c7
philemonheng <phil.hsy@outlook.com>
2021-07-21 13:43:36 +0800

    make Open Sans default

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
 import Head from 'next/head'
-import GoogleFonts from '../components/googleFonts'
 
 const Home = () => {
   return (
     <div>
       <Head>
         <title>about me</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       about me
     </div>
